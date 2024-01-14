232a4d1d699a84e9f525ee6326356b7e04bb8ac8
Thepython555 <144301348+thepython555@users.noreply.github.com>
2024-01-14 18:23:33 -0500

    this wil destroy the server LMAO

--- src/validators.js
+++ src/validators.js
@@ -3,13 +3,13 @@ const naughty = require('./naughty');
 /** List of possible prefixes that must appear at the beginning of all variable's names. */
 const CLOUD_PREFIXES = ['☁ ', ':cloud: '];
 /** The maximum length of a variable's name. Scratch does not seem to restrict this but we don't want overly long variable names regardless. */
-const VARIABLE_NAME_MAX_LENGTH = 1024;
+const VARIABLE_NAME_MAX_LENGTH = 10240;
 
 /** The maximum length of a variable's value. */
-const VALUE_MAX_LENGTH = 100000;
+const VALUE_MAX_LENGTH = 100000000;
 
 /** Maximum length of usernames, inclusive. */
-const USERNAME_MAX_LENGTH = 20;
+const USERNAME_MAX_LENGTH = 999;
 /** Minimum length of usernames, inclusive. */
 const USERNAME_MIN_LENGTH = 1;
 /** Regex for usernames to match. Letters, numbers, -, and _ */
